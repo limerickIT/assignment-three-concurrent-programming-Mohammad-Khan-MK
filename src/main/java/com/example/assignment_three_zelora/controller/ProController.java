ecba8d8a39187d3ea9a80aa6611a79aa60d96fc7
Mohammad Khan <K00133473@student.tus.ie>
2025-11-20 13:36:50 +0000

    added  search form and product listing page.

--- src/main/java/com/example/assignment_three_zelora/controller/ProController.java
+++ src/main/java/com/example/assignment_three_zelora/controller/ProController.java
@@ -1,11 +1,16 @@
 package com.example.assignment_three_zelora.controller;
 
 
+import com.example.assignment_three_zelora.model.dto.ProductSearch;
+import com.example.assignment_three_zelora.model.entitys.Category;
+import com.example.assignment_three_zelora.model.entitys.Product;
 import com.example.assignment_three_zelora.model.repos.CategoryRepository;
 import com.example.assignment_three_zelora.model.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ProController {
@@ -20,9 +25,31 @@ public class ProController {
 
     @GetMapping("/products")
     public String showProducts(Model model) {
+        ProductSearch form = new ProductSearch();
+        List<Category> categories = categoryRepository.findAll();
 
+        model.addAttribute("form", form);
+        model.addAttribute("categories", categories);
         model.addAttribute("products", productService.getAllProducts());
-        return "products/search"; // simple page
+
+        return "products/search";
+
+
     }
+    @GetMapping("/products/search")
+    public String searchProducts(ProductSearch form, Model model) {
+
+        System.out.println("Search Name: " + form.getName());
+        System.out.println("Search Category: " + form.getCategory());
+
+        List<Category> categories = categoryRepository.findAll();
+
+        model.addAttribute("form", form);
+        model.addAttribute("categories", categories);
+        model.addAttribute("products", productService.getAllProducts());
+
+        return "products/search";
+    }
+
 }
 
